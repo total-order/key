92ba25247ac7043b4014fd09738deaa4e4bd074c
make-github-pseudonymous-again <make-github-pseudonymous-again@users.noreply.github.com>
2021-07-14 16:56:50 +0000

    Deploying to gh-pages from @ total-order/key@69c9fe34008ab746fb291b36695a0fab384b988c 🚀

--- inject/script/0-header.js
+++ inject/script/0-header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/total-order/key';
+	testlink.href = 'https://app.codecov.io/gh/total-order/key';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
