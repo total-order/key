14c0e77866558b3310cf26b65b61dab477b9ebd0
a flying potato <80830782+a-flying-potato@users.noreply.github.com>
2024-02-09 22:34:49 +0100

    :robot: chore: Lint source files.
    These changes were automatically generated by a transform whose code can be found at:
      - https://github.com/make-github-pseudonymous-again/rejuvenate/blob/5cd83ef81aaefb6f6fc541d00cc7c4791d7b4c1c/src/transforms/sources:initial-lint.js
    Please contact the author of the transform if you believe there was an error.

--- src/len.js
+++ src/len.js
@@ -5,6 +5,6 @@
  * @param {Function} compare The function used to order lengths.
  * @return {Function} The function that orders objects by length.
  */
-// eslint-disable-next-line unicorn/prevent-abbreviations
+
 const len = (compare) => (a, b) => compare(a.length, b.length);
 export default len;
